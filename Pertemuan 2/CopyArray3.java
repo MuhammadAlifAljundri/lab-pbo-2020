46478c79ea4d9de1417a5b2c766f075bbc75599b
Mohd. Alif Al Jundri <m.alifaljundri@gmail.com>
2020-10-17 00:28:42 +0700

    Kode Pertemuan 2

---
+++ Pertemuan 2/CopyArray3.java
@@ -0,0 +1,11 @@
+public class CopyArray3 {
+	public static void main(String args[]) {
+		String str1 = "Informatika Universitas Syiah Kuala";
+		System.out.println(str1.startsWith("Inf"));
+		System.out.println(str1.endsWith("kuala"));
+		System.out.println(str1.startsWith("Inf",1));
+		System.out.println(str1.startsWith("Inf",2));
+		System.out.println(str1.startsWith("Inf",3));
+	}
+}
+//program di atas untuk menguji awalan atau akhiran suatu string berntebet
---
+++ Pertemuan 2/GetArguments.java
@@ -0,0 +1,8 @@
+public class GetArguments{
+	public static void main(String args[]){
+		System.out.println("Tanggal : " + args[0]);
+		System.out.println("Bulan   : " + args[1]);
+		System.out.println("Tahun   : " + args[2]);
+		
+	}
+}
