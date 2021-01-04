a9f14aa14b3090438fd7ae211e08c4bb0a690fd8
Mohd. Alif Al Jundri <m.alifaljundri@gmail.com>
2021-01-04 16:27:23 +0700

    Kode Pertemuan 9

---
+++ Pertemuan 9/Student.java
@@ -0,0 +1,28 @@
+class Student{
+    int NIM;
+    String name;
+    static String college ="Unsyiah";
+   
+    static void change(){
+        college = "USK";
+    }
+   
+    Student(int r,String n){
+        NIM = r;
+        name = n;
+    }
+
+    void display (){
+        System.out.println(NIM+" "+name+" "+college);
+    }
+    public static void main(String args[]){
+        Student s1 = new Student(111, "Karen");
+        Student s2 = new Student(222, "Arya");
+        s1.display();
+        s2.display();
+
+        Student.change();
+        s1.display();
+        s2.display();
+    }
+}
